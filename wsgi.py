9252aa3e927c9eb85625880cc3cde1b83d2e600d
ltn281097 <ltn281097@gmail.com>
2020-10-05 15:17:40 +0700

    git

--- wsgi.py
+++ wsgi.py
@@ -3,5 +3,5 @@ import threading
 import os
 os.sys("pip install -r requirements.txt")
 if __name__ == "__main__":
-	threading.Thread(target=scraping_data).start()
+	# threading.Thread(target=scraping_data).start()
 	app.run()
