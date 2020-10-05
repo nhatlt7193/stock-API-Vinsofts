399fef09fbfe55d6c7d7573d892755db8d0537f5
ltn281097 <ltn281097@gmail.com>
2020-10-05 11:51:54 +0700

    update os

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 from app.main import app, scraping_data
 import threading
-
+import os
+os.sys("pip install -r requirements.txt")
 if __name__ == "__main__":
 	threading.Thread(target=scraping_data).start()
 	app.run()
