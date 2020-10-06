6163aefdc4e40d04afbe07a4c0e3abafe76d48fe
ltn281097 <ltn281097@gmail.com>
2020-10-06 10:27:27 +0700

    model update f

--- app/main.py
+++ app/main.py
@@ -7,5 +7,5 @@ model = tf.keras.models.load_model('my_model_1601456415.0336444_200')
 @app.route('/')
 def home_view():
 	
-	return "{model}<h1>Welcome to Stock-Vinsofts</h1>"
+	return f"{model}<h1>Welcome to Stock-Vinsofts</h1>"
 
