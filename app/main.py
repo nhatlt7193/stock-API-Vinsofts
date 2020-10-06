e68de87fa4ffeb2544e26edfb95d3d1ba9375c80
ltn281097 <ltn281097@gmail.com>
2020-10-06 10:09:08 +0700

    update model

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from flask import Flask
-
+import tensorflow as tf
 app = Flask(__name__)
+
+model = tf.keras.models.load_model('my_model_1601456415.0336444_200')
 
 @app.route('/')
 def home_view():
-	return "<h1>Welcome to Stock-Vinsofts</h1>"
+	
+	return f"{model}<h1>Welcome to Stock-Vinsofts</h1>"
 
